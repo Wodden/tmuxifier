6511b8573adde3d3767961c76b02ad229788c388
Wo_denn? <wodden15@gmail.com>
2025-03-30 11:46:21 +0200

    init.

--- layouts/vvv.window.sh
+++ layouts/vvv.window.sh
@@ -8,16 +8,18 @@ new_window "vvv"
 
 # Split window into panes.
 split_h 20
-split_v 20
+split_v 30
 split_v 50
 
 # Run commands.
-run_cmd "watch -n 1 fish -c mp4_uniq" 3     # runs in active pane
-run_cmd "watch -wn 1 wc -l dList.lst" 4  # runs in pane 1
+run_cmd "dirs -c;cls" 1
+run_cmd "dirs -c;cls;ll" 2
+run_cmd "dirs -c;watch -n 1 fish -c mp4_uniq" 3     # runs in active pane
+run_cmd "dirs -c;watch -wn 1 wc -l dList.lst" 4  # runs in pane 1
 
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into pane 1
 
 # Set active pane.
-select_pane 0
+select_pane 1
